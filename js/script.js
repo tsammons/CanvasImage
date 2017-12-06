93dca9bc3f1a07b37b94e05a3b78453ec1fa0493
Tyler Sammons <tyler.sammons@perficient.com>
2017-12-06 14:09:23 -0600

    update

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@
 var ctx, canvas;
 var loaded = 0, width, height, x1, y1;
 var url1 = "https://media-cdn.tripadvisor.com/media/photo-s/02/91/0a/02/overcast-but-still-gorgeous.jpg";
-
+var url2 = "https://t4.ftcdn.net/jpg/01/83/13/45/500_F_183134585_y9rh5hQkT5gC2ujaes7kK8aamZ2nSwaH.jpg";
+var url3 = "http://www.media4.hw-static.com/wp-content/uploads/the-hangover-movie-stills_5641023-400x305.jpeg";
+var url4 = "https://stampsy-eu-1.s3.amazonaws.com/uploads/05/23/2015/user/21233/e1ce0a07-4151-4663-8f68-3c8d74da1dc4.jpg";
 
 function init() {
     document.addEventListener("click", printMousePos);
@@ -24,7 +26,7 @@ window.onresize = function() {
 function make_base()
 {
     var base_image = new Image();
-    base_image.src = url1 + '?' + new Date().getTime();
+    base_image.src = url4 + '?' + new Date().getTime();
     base_image.setAttribute('crossOrigin', '');
 
     base_image.onload = function(){
@@ -33,7 +35,7 @@ function make_base()
 	    x1 = Math.floor(canvas.width/2) - Math.floor(width/2);
 	    y1 = Math.floor(canvas.height/2) - Math.floor(height/2);
 	    ctx.drawImage(base_image, x1, y1);
-	    imageData(width, height, x1, y1);
+        imageData(width, height, x1, y1);
     }
 }
 
@@ -46,7 +48,7 @@ function imageData(w, h, x, y) {
     var pix = imgd.data;
 
     for (var i = 0, n = pix.length; i < n; i += 4) {
-	pix[i] = pix[i]; //0;
+	pix[i] = pix[i];
 	pix[i+1] = pix[i+1];
 	pix[i+2] = pix[i+2];
 	if (Math.random() > 1)
